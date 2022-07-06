ac49b9caf54ac3d0bc05732dd369f7d3c6f89135
Diego Gama <ti.diegogama@gmail.com>
2022-07-05 23:40:51 -0300

    projetos iniciais

---
+++ product-kafka-to-mongodb/src/main/java/com/product/ProductKafkaToMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductKafkaToMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductKafkaToMongodbApplication.class, args);
+	}
+
+}
